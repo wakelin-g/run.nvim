f688aead1e5f3e66d072031551ede4d354077359
Griffen Wakelin <wakelin@dal.ca>
2024-02-29 06:49:44 -0400

    initial draft

--- lua/run/init.lua
+++ lua/run/init.lua
@@ -2,37 +2,13 @@ local utils = require("run.utils")
 
 local M = {}
 
-function M.init()
-	local ft = {}
-end
-
-local function attach(bufnr, pattern, command)
-	vim.api.nvim_create_autocmd("BufWritePost", {
-		pattern = pattern,
-		group = vim.api.nvim_create_augroup("run", { clear = true }),
-		callback = function()
-			local append_data = function(_, data)
-				if data then
-					vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-				end
-			end
-
-			vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output:" })
-			vim.fn.jobstart(command, {
-				stdout_buffered = true,
-				on_stdout = append_data,
-				on_stderr = append_data,
-			})
-		end,
-	})
-end
-
 local default_run_commands = {
 	["c"] = "clang " .. vim.fn.expand("%") .. " && ./a.out",
 	["cpp"] = "clang++ " .. vim.fn.expand("%") .. " && ./a.out",
 	["py"] = "python3 " .. vim.fn.expand("%"),
 	["rs"] = "cargo run",
 	["go"] = "go run " .. vim.fn.expand("%"),
+	["lua"] = "lua " .. vim.fn.expand("%"),
 }
 
 M.run_commands = default_run_commands
@@ -41,19 +17,21 @@ function M.get_command(bufnr)
 	local ft = utils.get_ft(bufnr)
 	for ft_match, command in pairs(default_run_commands) do
 		if ft_match == ft then
-			return command -- command["run"]
+			return command
 		end
 	end
 	vim.notify("[run.nvim]: Could not find the `run` command for this filetype.")
 end
 
-function M.setup(config)
-	if config then
-		M.run_commands = vim.tbl_deep_extend("force", vim.deepcopy(M.run_commands), config)
-	end
+function M.run_job(command, callback)
+	vim.fn.jobstart(command, {
+		stdout_buffered = true,
+		on_stdout = callback,
+		on_stderr = callback,
+	})
 end
 
-function M.testrun()
+function M.run()
 	local buf = vim.api.nvim_get_current_buf()
 	local cmd = M.get_command(buf)
 	local out = utils.create_buf()
@@ -63,11 +41,25 @@ function M.testrun()
 			vim.api.nvim_buf_set_lines(out, -1, -1, false, data)
 		end
 	end
-	vim.api.nvim_open_win(0, false, { split = "above", win = 0 })
-	vim.fn.jobstart(cmd, {
-		stdout_buffered = true,
-		on_stdout = append_data,
-		on_stderr = append_data,
+	local win = vim.api.nvim_open_win(out, true, { split = "right", win = 0, style = "minimal" })
+	vim.api.nvim_win_set_width(win, 40)
+	M.run_job(cmd, append_data)
+	M.buffer_attach(vim.api.nvim_win_get_buf(win), cmd)
+end
+
+function M.buffer_attach(bufnr, command)
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = vim.api.nvim_create_augroup("run-main", { clear = true }),
+		callback = function()
+			local append_data = function(_, data)
+				if data then
+					vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+				end
+			end
+
+			vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "output:" })
+			M.run_job(command, append_data)
+		end,
 	})
 end
 
--- plugin/run.lua
+++ plugin/run.lua
@@ -4,6 +4,6 @@ end
 
 vim.g.load_run = true
 
--- vim.api.nvim_create_user_command("RunToggle", function(args)
--- 	require("run")
--- end)
+vim.api.nvim_create_user_command("Run", function()
+	require("run").run()
+end, { silent = true, desc = "run current file" })
