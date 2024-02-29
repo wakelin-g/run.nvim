6ef341062d059f3baa76edfb83a4832cc81693de
Griffen Wakelin <wakelin@dal.ca>
2024-02-29 07:09:12 -0400

    fixed default commands not being used

--- lua/run/init.lua
+++ lua/run/init.lua
@@ -2,7 +2,7 @@ local utils = require("run.utils")
 
 local M = {}
 
-local default_run_commands = {
+M.run_commands = {
 	["c"] = "clang " .. vim.fn.expand("%") .. " && ./a.out",
 	["cpp"] = "clang++ " .. vim.fn.expand("%") .. " && ./a.out",
 	["py"] = "python3 " .. vim.fn.expand("%"),
@@ -11,11 +11,13 @@ local default_run_commands = {
 	["lua"] = "lua " .. vim.fn.expand("%"),
 }
 
-M.run_commands = default_run_commands
-
 function M.get_command(bufnr)
-	local ft = utils.get_ft(bufnr)
-	for ft_match, command in pairs(default_run_commands) do
+	local ft = function()
+		if bufnr ~= nil then
+			return vim.filetype.match({ buf = bufnr })
+		end
+	end
+	for ft_match, command in pairs(M.run_commands) do
 		if ft_match == ft then
 			return command
 		end
@@ -29,22 +31,6 @@ function M.run_job(command, callback)
 		on_stdout = callback,
 		on_stderr = callback,
 	})
-end
-
-function M.run()
-	local buf = vim.api.nvim_get_current_buf()
-	local cmd = M.get_command(buf)
-	local out = utils.create_buf()
-	vim.api.nvim_buf_set_lines(out, 0, -1, false, { "output:" })
-	local append_data = function(_, data)
-		if data then
-			vim.api.nvim_buf_set_lines(out, -1, -1, false, data)
-		end
-	end
-	local win = vim.api.nvim_open_win(out, true, { split = "right", win = 0, style = "minimal" })
-	vim.api.nvim_win_set_width(win, 40)
-	M.run_job(cmd, append_data)
-	M.buffer_attach(vim.api.nvim_win_get_buf(win), cmd)
 end
 
 function M.buffer_attach(bufnr, command)
@@ -61,6 +47,28 @@ function M.buffer_attach(bufnr, command)
 			M.run_job(command, append_data)
 		end,
 	})
+	vim.notify("[run.nvim]: Attached to buffer.")
+end
+
+function M.run()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local command = M.get_command(bufnr)
+	local bufnr_out = utils.create_buf()
+	vim.api.nvim_buf_set_lines(bufnr_out, 0, -1, false, { "output:" })
+	local append_data = function(_, data)
+		if data then
+			vim.api.nvim_buf_set_lines(bufnr_out, -1, -1, false, data)
+		end
+	end
+	local win = vim.api.nvim_open_win(bufnr_out, true, { split = "right", win = 0, style = "minimal" })
+	vim.api.nvim_win_set_width(win, 40)
+	M.run_job(command, append_data)
+	M.buffer_attach(vim.api.nvim_win_get_buf(win), command)
+end
+
+function M.setup(commands)
+	commands = commands or {}
+	M.run_commands = vim.tbl_deep_extend("force", commands, M.run_commands)
 end
 
 return M
--- plugin/run.lua
+++ plugin/run.lua
@@ -6,4 +6,6 @@ vim.g.load_run = true
 
 vim.api.nvim_create_user_command("Run", function()
 	require("run").run()
-end, { silent = true, desc = "run current file" })
+end, { desc = "run current file", force = false })
+
+vim.keymap.set("n", "<C-b>", "<cmd>Run<CR>", { silent = true, noremap = true, desc = "run current file" })
