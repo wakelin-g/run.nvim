e96898a10fc56c80156557901eede23f1e8bfa07
Griffen Wakelin <wakelin@dal.ca>
2024-03-01 20:32:24 -0400

    added ability to choose from multiple commands

--- lua/run/init.lua
+++ lua/run/init.lua
@@ -3,12 +3,12 @@ local M = {}
 local u = require("run.utils")
 
 local default_run_commands = {
-	["c"] = "clang " .. vim.fn.expand("%") .. " && ./a.out",
-	["cpp"] = "clang++ " .. vim.fn.expand("%") .. " && ./a.out",
-	["python"] = "python " .. vim.fn.expand("%"),
-	["rs"] = "cargo run",
-	["go"] = "go run " .. vim.fn.expand("%"),
-	["lua"] = "lua " .. vim.fn.expand("%"),
+	["c"] = { "clang " .. vim.fn.expand("%") .. " && ./a.out" },
+	["cpp"] = { "clang++ " .. vim.fn.expand("%") .. " && ./a.out" },
+	["python"] = { "python3 " .. vim.fn.expand("%") },
+	["rs"] = { "cargo run" },
+	["go"] = { "go run " .. vim.fn.expand("%") },
+	["lua"] = { "lua " .. vim.fn.expand("%") },
 }
 
 M.config = {}
@@ -20,16 +20,44 @@ function M.setup(args)
 	end
 	M.config.win_width = tonumber(args.win_width) or 40
 	M.config.output_msg = args.output_msg or " -- OUTPUT -- "
-	M.config.run_commands = vim.tbl_deep_extend("force", args.commands or {}, default_run_commands)
+
+	for arg_key, arg_val in pairs(args.commands or {}) do
+		if default_run_commands[arg_key] == nil then
+			default_run_commands[arg_key] = arg_val
+		else
+			for _, val in pairs(arg_val) do
+				table.insert(default_run_commands[arg_key], val)
+			end
+		end
+	end
+	M.config.run_commands = default_run_commands
+	-- M.config.run_commands = vim.tbl_deep_extend("force", args.commands or {}, default_run_commands)
 end
 
 function M.get_command(ft)
 	for ft_match, command in pairs(M.config.run_commands) do
 		if ft_match == ft then
-			return command
+			if #command == 1 then
+				return command[1]
+			else
+				local selected
+				vim.ui.select(command, {
+					prompt = "Select command to run: ",
+					format_item = function(item)
+						return ft .. " : " .. item
+					end,
+				}, function(choice)
+					selected = choice
+				end)
+				return selected
+			end
 		end
 	end
 	vim.notify("[run.nvim]: Could not find the `run` command for this filetype.")
+end
+
+function M.get_command_list()
+	return M.config.run_commands
 end
 
 function M.run_job(command, callback)
@@ -45,7 +73,7 @@ function M.is_available(ft)
 end
 
 local function buffer_attach(bufnr, command)
-	vim.api.nvim_create_autocmd("BufWritePost", {
+	vim.b.run_autocmd = vim.api.nvim_create_autocmd("BufWritePost", {
 		group = vim.api.nvim_create_augroup("run-main", { clear = true }),
 		callback = function()
 			local append_data = function(_, data)
@@ -60,6 +88,13 @@ local function buffer_attach(bufnr, command)
 	})
 end
 
+function M.change_command()
+	local ft = vim.api.nvim_get_current_buf()
+	vim.b.run_buf_command = M.get_command(vim.filetype.match({ buf = ft }))
+	vim.api.nvim_del_autocmd(vim.b.run_autocmd)
+	buffer_attach(vim.api.nvim_win_get_buf(vim.b.run_win), vim.b.run_buf_command)
+end
+
 function M.main()
 	local bufnr_cur = vim.api.nvim_get_current_buf()
 	local ft = vim.filetype.match({ buf = bufnr_cur })
@@ -69,24 +104,25 @@ function M.main()
 		return
 	end
 
-	local command = M.get_command(ft)
-	local bufnr_out = vim.api.nvim_create_buf(false, true)
+	if vim.b.run_buf_handle == nil then
+		vim.b.run_buf_handle = vim.api.nvim_create_buf(false, true)
+		local bufnr_out = vim.b.run_buf_handle
+		vim.b.run_buf_command = M.get_command(ft)
 
-	vim.api.nvim_set_option_value("filetype", "runwin", { buf = bufnr_out })
-	vim.api.nvim_buf_set_lines(bufnr_out, 0, -1, false, { M.config.output_msg, "" })
+		vim.api.nvim_set_option_value("filetype", "runwin", { buf = bufnr_out })
+		vim.api.nvim_buf_set_lines(bufnr_out, 0, -1, false, { M.config.output_msg, "" })
 
-	local append_data = function(_, data)
-		if data then
-			vim.api.nvim_buf_set_lines(bufnr_out, -1, -1, false, data)
-		end
+		vim.b.run_win = vim.api.nvim_open_win(bufnr_out, false, { split = "right", win = 0, style = "minimal" })
+		vim.api.nvim_win_set_width(vim.b.run_win, M.config.win_width)
+		buffer_attach(vim.api.nvim_win_get_buf(vim.b.run_win), vim.b.run_buf_command)
 	end
 
-	local win = vim.api.nvim_open_win(bufnr_out, false, { split = "right", win = 0, style = "minimal" })
-	vim.api.nvim_win_set_width(win, M.config.win_width)
-
-	M.run_job(command, append_data)
-
-	buffer_attach(vim.api.nvim_win_get_buf(win), command)
+	vim.api.nvim_buf_set_lines(vim.b.run_buf_handle, 0, -1, false, { M.config.output_msg, "" })
+	M.run_job(vim.b.run_buf_command, function(_, data)
+		if data then
+			vim.api.nvim_buf_set_lines(vim.b.run_buf_handle, -1, -1, false, data)
+		end
+	end)
 end
 
 return M
